ffbc1d8c142015ae8095b44a016d0a2bd2fd3424
wrd1stProgrammer <kicoa24@gmail.com>
2025-07-11 18:43:38 +0900

    server

--- front/src/redux/config/API.tsx
+++ front/src/redux/config/API.tsx
@@ -1,7 +1,7 @@
 
 // FOR LOCAL
 
-export const BASE_URL = `http://13.125.195.166:5001`
+export const BASE_URL = `https://ballorun.com`
   
 // RUNNING ON REAL DEVICE USE YOUR NETWORK IP TO ACCESS ON REAL DEVICE
 //eg http://192.168.29.88:3000
